51f5511bee144d1410bc9d6463159699ec7558a7
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-12-10 21:41:34 +0300

    feat!: implement initial persona fusion function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,19 @@ import { fuse } from "@/lib/fuse";
 import { personae } from "@/config/data";
 
 export default function Home() {
+  const persona1 = personae.find((persona) => {
+    return persona.name === 'Angel';
+  });
+  const persona2 = personae.find((persona) => {
+    return persona.name === 'Izanagi';
+  });
+console.log(persona1, persona2)
   return (
     <div>
       Initial
       <button 
         className={"p-2 bg-red-400 rounded text-white"}
-        onClick={() => fuse(personae[0].name)}
+        onClick={() => fuse("Magician", persona1, persona2)}
       >
         Fuse
       </button>
--- src/lib/fuse.ts
+++ src/lib/fuse.ts
@@ -1,5 +1,33 @@
-export function fuse(...args: string[]) {
-    console.log(args);
+import { personae } from "@/config/data";
 
-    return;
+const personaeByArcana = (function() {
+    var personaeByArcana_: any = {};
+    for (var i = 0, persona = null; persona = personae[i]; i++) {
+      if (!personaeByArcana_[persona.arcana]) {
+        personaeByArcana_[persona.arcana] = [];
+      }
+      personaeByArcana_[persona.arcana].push(persona);
+    }
+    return personaeByArcana_;
+  })();
+
+export function fuse(arcana: string, persona1: any, persona2: any) {
+    var level = 1 + Math.floor((persona1.level + persona2.level) / 2);
+    var personae = personaeByArcana[arcana];
+
+    for (var i = 0, persona = null; persona = personae[i]; i++) {
+        if (persona.level >= level) {
+        if (persona.special) continue;
+        break;
+        }
+    }
+
+    if (persona1.arcana == persona2.arcana) {
+        i--;
+    }
+    if (personae[i] == persona1 || personae[i] == persona2) {
+        i--;
+    }
+
+    return personae;
 }
